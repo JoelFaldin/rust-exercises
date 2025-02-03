96b9475a5d41a92907d86e3936bdf31a4de3feb8
Joel Faldin <joelfaldin@gmail.com>
2025-02-02 21:01:20 -0300

    feat: integer numbers types

---
+++ rust-numbers/src/main.rs
@@ -0,0 +1,53 @@
+fn main() {
+    // Unsigned integers:
+    let u8_min: u8 = u8::MIN;
+    let u8_max: u8 = u8::MAX;
+    println!("Min value of u8 is {u8_min} and max value is {u8_max}");
+
+    let u16_min: u16 = u16::MIN;
+    let u16_max: u16 = u16::MAX;
+    println!("Min value of u16 is {u16_min} and max value is {u16_max}");
+
+    let u32_min: u32 = u32::MIN;
+    let u32_max: u32 = u32::MAX;
+    println!("Min value of u32 is {u32_min} and max value is {u32_max}");
+
+    let u64_min: u64 = u64::MIN;
+    let u64_max: u64 = u64::MAX;
+    println!("Min value of u64 is {u64_min} and max value is {u64_max}");
+
+    let u128_min: u128 = u128::MIN;
+    let u128_max: u128 = u128::MAX;
+    println!("Min value of u128 is {u128_min} and max value is {u128_max}");
+
+    let usize_min: usize = usize::MIN;
+    let usize_max: usize = usize::MAX;
+    println!("Min value of usize is {usize_min} and max value is {usize_max}");
+
+    println!("----------------------------------------------------------------");
+    
+    // Signed integers:
+    let i8_min: i8 = i8::MIN;
+    let i8_max: i8 = i8::MAX;
+    println!("Min value of i8 is {i8_min} and max value is {i8_max}");
+
+    let i16_min: i16 = i16::MIN;
+    let i16_max: i16 = i16::MAX;
+    println!("Min value of i16 is {i16_min} and max value is {i16_max}");
+
+    let i32_min: i32 = i32::MIN;
+    let i32_max: i32 = i32::MAX;
+    println!("Min value of i32 is {i32_min} and max value is {i32_max}");
+
+    let i64_min: i64 = i64::MIN;
+    let i64_max: i64 = i64::MAX;
+    println!("Min value of i64 is {i64_min} and max value is {i64_max}");
+
+    let i128_min: i128 = i128::MIN;
+    let i128_max: i128 = i128::MAX;
+    println!("Min value of i128 is {i128_min} and max value is {i128_max}");
+
+    let isize_min: isize = isize::MIN;
+    let isize_max: isize = isize::MAX;
+    println!("Min value of isize is {isize_min} and max value is {isize_max}");
+}
