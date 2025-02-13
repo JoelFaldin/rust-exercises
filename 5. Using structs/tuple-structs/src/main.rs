fc49e08bcd9a2782f19562e757cb04f88d2291f5
Joel Faldin <joelfaldin@gmail.com>
2025-02-12 23:27:20 -0300

    feat: adding structs and tuple-structs exercises

---
+++ 5. Using structs/tuple-structs/src/main.rs
@@ -0,0 +1,14 @@
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+fn main() {
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+}
+
+// Unit-like structs:
+struct AlwaysEqual;
+
+fn main2() {
+    let subject = AlwaysEqual;
+}
