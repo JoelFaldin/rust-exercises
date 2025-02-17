d3a1a0c405ed9de252d13a01d5270afd4b231418
Joel Faldin <joelfaldin@gmail.com>
2025-02-16 23:28:31 -0300

    feat: adding concatenating strings exercises

--- 8. Common collections/strings/src/main.rs
+++ 8. Common collections/strings/src/main.rs
@@ -25,5 +25,26 @@ fn main() {
 
             println!("{s3}");
         }
+
+        // Concatenating strings:
+        {
+            let s1 = String::from("hello");
+            let s2 = String::from(" world!");
+            let s3 = s1 + &s2;
+
+            println!("{s3}");
+        }
+
+        {
+            // Alternative (using format!):
+            {
+                let s1 = String::from("hello");
+                let s2 = String::from("rust");
+                let s3 = String::from("world!");
+
+                let s4 = format!("{s1} {s2} {s3}");
+                println!("{s4}");
+            }
+        }
     }
 }
