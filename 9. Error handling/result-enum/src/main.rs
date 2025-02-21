3437bac21121f6f3c210aeeeea6ee4054acd66a8
JoelFaldin <joelfaldin@gmail.com>
2025-02-20 23:45:32 -0300

    feat: adding matching different errors

---
+++ 9. Error handling/result-enum/src/main.rs
@@ -0,0 +1,19 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
+fn main() {
+    let greeting_file_result = File::open("test.txt");
+
+    let _reeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("test.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error)
+            }
+        },
+    };
+}
