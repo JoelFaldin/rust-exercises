c174c40563b41c04f95e4eacf9e038b214111fdd
JoelFaldin <joelfaldin@gmail.com>
2025-02-21 13:11:58 -0300

    feat: adding improved guessing game

---
+++ 9. Error handling/fixed-guessing-game/src/main.rs
@@ -0,0 +1,54 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got '{value}'!");
+        }
+        println!("u good mate");
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
+fn main() {
+    println!("Guess the number dude!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Input your guess here: ");
+    
+        let mut guess = String::new();
+    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line :C");
+    
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        Guess::new(guess);
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                print!("You won bro :D");
+                break;
+            },
+        }
+    }
+}
