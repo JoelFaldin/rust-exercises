da8a8a8483ca59c9fc2f7508821e08ed58a32fc0
JoelFaldin <joelfaldin@gmail.com>
2025-02-23 00:22:08 -0300

    feat: adding largest char function

--- 10. Generic Types, Traits and Lifetimes/largest-number/src/main.rs
+++ 10. Generic Types, Traits and Lifetimes/largest-number/src/main.rs
@@ -10,14 +10,26 @@ fn largest(list: &[i32]) -> &i32 {
     largest
 }
 
+fn largest_char(list: &[char]) -> &char {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
     let result = largest(&number_list);
     println!("Largest number is: {result}");
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let char_list = vec!['y', 'm', 'p', 'a'];
     
-    let result = largest(&number_list);
-    println!("The largest number is {result}");
+    let result = largest_char(&char_list);
+    println!("The largest char is {result}");
 }
