9ac853e6b81e0fa9134df4a7c2549086fb306e3b
JoelFaldin <joelfaldin@gmail.com>
2025-02-25 14:37:18 -0300

    feat: adding specific method when a struct is of one type

--- 10. Generic Types, Traits and Lifetimes/generics-in-structs/src/main.rs
+++ 10. Generic Types, Traits and Lifetimes/generics-in-structs/src/main.rs
@@ -9,6 +9,12 @@ impl<T> Point<T> {
     }
 }
 
+impl Point<f32> {
+    fn _distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
 fn main() {
     let _integer = Point { x: 5, y: 10 };
     let _float = Point { x: 1.0, y: 4.0 };
