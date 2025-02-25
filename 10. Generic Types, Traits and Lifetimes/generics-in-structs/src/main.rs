2b0730808b5abab0899e34583284c14c83f2056e
JoelFaldin <joelfaldin@gmail.com>
2025-02-24 23:27:01 -0300

    feat: adding new struct with different generics

--- 10. Generic Types, Traits and Lifetimes/generics-in-structs/src/main.rs
+++ 10. Generic Types, Traits and Lifetimes/generics-in-structs/src/main.rs
@@ -7,3 +7,14 @@ fn main() {
     let _integer = Point { x: 5, y: 10 };
     let _float = Point { x: 1.0, y: 4.0 };
 }
+
+struct Point_2<T, U> {
+    x: T,
+    y: U,
+}
+
+fn _main2() {
+    let _both_integer = Point_2 { x: 5, y: 10 };
+    let _both_float = Point_2 { x: 1.0, y: 4.0 };
+    let _integer_and_float = Point_2 { x: 5, y: 4.0 };
+}
